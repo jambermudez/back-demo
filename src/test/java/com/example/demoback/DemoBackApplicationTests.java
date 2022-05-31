44886f2b625321ac4cbe69a2a7a9494b5596bab0
jamartin <josemartinbermudez@gmail.com>
2022-05-31 17:28:32 +0200

    Start proyect | We initialize the project from https://start.spring.io/ with dependencies Spring Web and Spring Boot DevTools

---
+++ src/test/java/com/example/demoback/DemoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
